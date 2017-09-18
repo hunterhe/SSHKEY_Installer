fe26e3ee0f5f7d5f4c1829f55828fa767882805c
hunterhe <hxllovewd@gmail.com>
2017-09-18 11:46:37 +0800

    Update key.sh

--- key.sh
+++ key.sh
@@ -1,10 +1,9 @@
-#/bin/csh
+#/bin/sh
 echo '============================
       SSH Key Installer
 	      V1.0 Alpha
 		Author:Kirito
 ============================'
-cd ~
 mkdir .ssh
 cd .ssh
 echo 'ssh-rsa AAAAB3NzaC1yc2EAAAABIwAAAQEAqlnNLNW7njuimrjRklgVZLpExdtjlNLgZ2yfyd2X2LLs46k7owZeYCA269JvfyHVa97sAugc5mzn+kx9SPQClqtJqRYW2GQsRfLJevuDQ2ygHURhxgBbeF0ztckh44RKUL355aMGBEQFdMxZCMmS3L/JSirCmfBoG6BD2W8M7KrsxRpgURm/sUGYP10JCUagDpGAU3TBZJetpHaU71imNdeE6Tt45R/pazn5MDhZAXPANbGI/iK22w/rTXqOCS76+pNTcw2Mo0VnZhexqsCj3C5JvbgV7+5o0YXa0u5/AqS5dpzohomn4U0V3HpIn+fv4e448GZW5Ff6c7ZVw49L2w== rsa 2048-060717' > authorized_keys
